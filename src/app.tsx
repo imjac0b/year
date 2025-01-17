da64d3d45dae82c6897b3926312b3a165c1490dd
imjac0b <186802484+imjac0b@users.noreply.github.com>
2025-01-17 11:04:23 +0800

    done

--- src/app.tsx
+++ src/app.tsx
@@ -1,42 +1,50 @@
-import { useState } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
-import viteLogo from "/vite.svg";
+import { computed, signal } from "@preact/signals";
+import { cn } from "./lib/utils";
+
+const isLeapYear = (year: number): boolean => {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+};
+
+const getDayOfYear = (date: Date): number => {
+  const start = new Date(date.getFullYear(), 0, 0);
+  const diff = date.getTime() - start.getTime();
+  const oneDay = 1000 * 60 * 60 * 24;
+  return Math.floor(diff / oneDay);
+};
+
+const showDaysPassed = signal(false);
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const year = computed(() => new Date().getFullYear());
+  const daysInYear = computed(() => (isLeapYear(year.value) ? 366 : 365));
+  const daysPassed = computed(() => getDayOfYear(new Date()));
+  const daysLeft = computed(() => daysInYear.value - daysPassed.value);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+    <main className="flex flex-col items-center justify-center h-screen p-8 gap-8">
+      <div className="flex flex-wrap gap-4 md:gap-6 lg:gap-8">
+        {Array.from({ length: daysInYear.value }, (_, i) => (
+          <div
+            key={i}
+            className={cn(
+              "w-1 h-1 rounded-full",
+              i < daysPassed.value ? "bg-white" : "bg-muted"
+            )}
+          ></div>
+        ))}
       </div>
-      <h1 className="text-3xl font-bold underline">Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className={"flex justify-between w-full"}>
+        <p>{year}</p>
+        <button onClick={() => (showDaysPassed.value = !showDaysPassed.value)}>
+          <span>
+            {showDaysPassed.value ? daysLeft.value : daysPassed.value}
+          </span>
+          <span className="text-muted-foreground">
+            {" "}
+            days {showDaysPassed.value ? "left" : "passed"}
+          </span>
         </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p>
-        Check out{" "}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { render } from "preact";
 import "./index.css";
+import "@fontsource-variable/geist-mono";
 import { App } from "./app.tsx";
 
 render(<App />, document.getElementById("app")!);
